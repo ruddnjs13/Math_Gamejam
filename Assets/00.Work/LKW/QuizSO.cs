2316234610386f944b362ca2cc6b4aca2a4ec55a
ruddnjs13 <ruddnjs013@gmail.com>
2024-09-30 00:50:22 +0900

    폰트추가

---
+++ Assets/00.Work/LKW/QuizSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Quiz")]
+public class QuizSO : ScriptableObject
+{
+   public string quizInfo;
+   public int answerIndex;
+}
